c31b3e30f226ff15952a388bc2a567db1417f766
Joe Fisher <josephfish1973@gmail.com>
2022-10-09 13:31:07 -0400

    indexdb

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -9,11 +9,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
-
+import {initdb} from './database.js';
 
 
 window.addEventListener('load', function () {
-    document.getElementById('logo').src = Logo;
-    document.getElementById('bearThumbnail').src = Bear;
-    document.getElementById('dogThumbnail').src = Dog;
+  initdb();
+  document.getElementById('logo').src = Logo;
+  document.getElementById('bearThumbnail').src = Bear;
+  document.getElementById('dogThumbnail').src = Dog;
+    
   });
