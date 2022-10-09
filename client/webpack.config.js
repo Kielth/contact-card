501e0ef12fefded4fa1ecacafa37039009a2364c
Joe Fisher <josephfish1973@gmail.com>
2022-10-09 19:39:09 -0400

    add to staging 4

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const WorkboxPlugin = require('workbox-webpack-plugin');
+const {InjectManifest} = require('workbox-webpack-plugin');
 
 module.exports = {
+  mode: 'develop',
   entry: './src/js/index.js',
   output: {
     filename: 'bundle.js',
@@ -37,8 +41,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './index.html',
       title: 'Webpack Plugin',
+    }),
+    new InjectManifest({
+      swSrc: './src/sw.js',
+      swDest: 'service-worker.js',
     })
-  ],
-
-  
+  ], 
 };
